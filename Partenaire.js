e53bd319f0822ccd7fc4ae5d60ae3168daa74392
Stella <90323575+stellaric@users.noreply.github.com>
2023-12-15 11:29:20 +0100

    Create Partenaire.js

---
+++ Partenaire.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Partenaire() {
+  return (
+    <section className='service_partenaire'>
+      
+      <h2>Nos Partenaires</h2>
+     <div className='partenaire_contenu'>
+    <div className='partenaire_form' id="loisir">ceci est le partenaire 1</div>
+    <div className='partenaire_form' id="repas">ceci est le partenaire 2 </div>
+    <div className='partenaire_form' id="garderie">ceci est le partenaire 3 </div>
+
+     </div>
+     
+    </section>
+  )
+}
